0a30632c46cb60ce25fafc33f81a6df873502d6b
consolelogfuku <fuku1223sugawa@gmail.com>
2023-07-03 18:07:10 +0900

    add:Ingredientモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_075835) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_085250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cooking_methods", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name_1"
+    t.string "name_2"
+    t.string "name_3"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
