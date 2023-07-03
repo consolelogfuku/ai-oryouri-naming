6c567c0999472f3412243be347f74e46039a72d9
consolelogfuku <fuku1223sugawa@gmail.com>
2023-07-03 22:33:40 +0900

    add:Ingredientモデルにアソシエーション、カスタムメソッドの追加

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,14 @@
 class Ingredient < ApplicationRecord
+  belongs_to :dish
+
+  # カスタムメソッド
+  validate :at_least_one_ingredient
+
+  private
+  
+  def at_least_one_ingredient
+    if name_1.blank? && name_2.blank? && name_3.blank?
+      errors.add(:base, "食材は一つ以上入力してください")
+    end
+  end
 end
