d5b3f39378afb2767f0e44c9733a8bf925c67854
consolelogfuku <fuku1223sugawa@gmail.com>
2023-07-25 15:50:49 +0900

    add:ユーザーからの入力値をDBに保存できるように設定

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -11,10 +11,14 @@ class DishesController < ApplicationController
   end
 
   def create
-    # @dish = current_user.dishes.build(dish_params)
-    #   if @dish.save
-
-    #   end
+    user = User.find(1)
+    # 料理を保存
+    @dish = user.dishes.build(dish_params)
+    if @dish.save_with_ingredients_and_cooking_methods(name_1: params.dig(:dish, :name_1), name_2: params.dig(:dish, :name_2), name_3: params.dig(:dish, :name_3), cooking_methods_name: params.dig(:dish, :cooking_methods_name))
+      redirect_to new_dish_path, notice: "料理を保存しました"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -34,6 +38,5 @@ class DishesController < ApplicationController
   def dish_params
     params.require(:dish).permit(:seasoning_id, :texture_id, :category_id, :point, :dish_image)
   end
-
 
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -7,8 +7,24 @@ class Dish < ApplicationRecord
   has_many :dishes_cooking_methods, dependent: :destroy
   has_many :cooking_methods, through: :dishes_cooking_methods
 
+  before_validation -> { self.uuid = SecureRandom.uuid }
   validates :uuid, presence: true
   validates :point, length: { maximum: 20 } # 20文字以内
   validates :state, presence: true
   enum state: { draft: 0, published: 1 }
+
+  # 食材と調理法も一緒に保存
+  def save_with_ingredients_and_cooking_methods(name_1:, name_2:, name_3:, cooking_methods_name:)
+    ActiveRecord::Base.transaction do
+      # 食材(同じ食材の組み合わせがあった場合は、その食材を取得する)
+      self.ingredient = Ingredient.find_or_create_by(name_1: name_1, name_2: name_2, name_3: name_3)
+      # 調理法
+      cooking_methods_name.each do |cooking_method_name|
+        self.cooking_methods << CookingMethod.find_by(name: cooking_method_name)
+      end
+      save!
+    end
+  end
+
 end
+
