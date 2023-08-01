f63440a15c973b9d616c566063311f358e35e5d9
consolelogfuku <fuku1223sugawa@gmail.com>
2023-08-01 21:29:47 +0900

    add:サインアップフォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root to: 'dishes#new'
+  get 'sign_up', to: 'users#new'
 
+  resources :users, param: :uuid, only: [:create, :show] 
   resources :dishes, param: :uuid do
     get 'result', on: :member
   end
