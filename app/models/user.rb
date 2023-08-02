a7cad0aad65ebf9162125dc7b42842950e995dc9
consolelogfuku <fuku1223sugawa@gmail.com>
2023-08-02 09:33:49 +0900

    add:User登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
   validates :uuid, presence: true
   validates :name, presence: true, length: { maximum: 20 } # 20文字以内
   validates :email, presence: true, uniqueness: true
-  
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
