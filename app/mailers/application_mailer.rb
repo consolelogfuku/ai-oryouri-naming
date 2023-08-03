09f176e49a70be3ff11cea791cdaa61441e4be70
consolelogfuku <fuku1223sugawa@gmail.com>
2023-08-03 21:22:15 +0900

    add:パスワードリセット画面の作成(リセット機能はsorceryを用いている)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "	ai.oryouri.naming@gmail.com" # 送信元アドレス
   layout "mailer"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :reset_password_token, uniqueness: true, allow_nil: true
 end
