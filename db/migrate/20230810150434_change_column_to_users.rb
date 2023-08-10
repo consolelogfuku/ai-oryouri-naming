a22bfc40bb931f822d8e27d2078e005825ec5e1a
consolelogfuku <fuku1223sugawa@gmail.com>
2023-08-11 00:09:40 +0900

    fix:Usersテーブルのuuidカラムにindexの追加・NotNull制約の解除

---
+++ db/migrate/20230810150434_change_column_to_users.rb
@@ -0,0 +1,10 @@
+class ChangeColumnToUsers < ActiveRecord::Migration[7.0]
+  def up
+    change_column :users, :uuid,:string, null: true
+    add_index :users, :uuid, unique: true
+  end
+
+  def down
+    change_column :users, :uuid,:string, null: false
+  end
+end
