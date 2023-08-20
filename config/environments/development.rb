8ec8f23ed65477de4c94b1290385fb89248f1a23
consolelogfuku <fuku1223sugawa@gmail.com>
2023-08-20 22:45:45 +0900

    add:本番環境でのメール設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,9 +46,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { protocol: 'http', host: Settings.host} # ローカルはHttp通信
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_ADDRESS'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -78,6 +88,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # letter_opener_web
-  config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { host: Settings.host }
+  # config.action_mailer.delivery_method = :letter_opener_web
+  # config.action_mailer.default_url_options = { host: Settings.host }
 end
