cf1f80540702d2f87d628d915c593de6f9d2e66c
consolelogfuku <fuku1223sugawa@gmail.com>
2023-09-18 13:20:54 +0900

    improve:不要なコードの削除、コメントの追加等

--- app/javascript/controllers/similar_dish_controller.js
+++ app/javascript/controllers/similar_dish_controller.js
@@ -5,14 +5,16 @@ export default class extends Controller {
   static targets = [ 'mask', 'modal', 'button', 'buttonText' ]
 
   showModal() {
-    this.buttonTarget.setAttribute('data-action', 'click->similar-dish#closeModal');  // data-actionを変更
+    // ボタンをおしたら、data-actionの値がcloseModalに変更されるよう設定
+    this.buttonTarget.setAttribute('data-action', 'click->similar-dish#closeModal');
     this.buttonTextTarget.textContent = '閉じる';
     this.maskTarget.classList.remove('hidden');
     this.modalTarget.classList.remove('hidden');
   };
 
   closeModal() {
-    this.buttonTarget.setAttribute('data-action', 'click->similar-dish#showModal');  // data-actionを元に戻す
+    // ボタンをおしたら、data-actionの値showModalに変更されるよう設定
+    this.buttonTarget.setAttribute('data-action', 'click->similar-dish#showModal');
     this.buttonTextTarget.textContent = '似た料理を見る';
     this.maskTarget.classList.add('hidden');
     this.modalTarget.classList.add('hidden');
