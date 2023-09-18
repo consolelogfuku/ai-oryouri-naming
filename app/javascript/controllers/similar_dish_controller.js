f19418710c36f77bea447cb1c99cea5e9ac9816e
consolelogfuku <fuku1223sugawa@gmail.com>
2023-09-18 09:06:29 +0900

    add:「似た料理」表示機能の実装

--- app/javascript/controllers/similar_dish_controller.js
+++ app/javascript/controllers/similar_dish_controller.js
@@ -2,10 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="similar-dish"
 export default class extends Controller {
+  static targets = [ 'mask', 'modal', 'button', 'buttonText' ]
 
   showModal(e) {
-    // const target = e.target
+    this.buttonTarget.setAttribute('data-action', 'click->similar-dish#closeModal');  // data-actionを変更
+    this.buttonTextTarget.textContent = '閉じる';
+    this.maskTarget.classList.remove('hidden');
+    this.modalTarget.classList.remove('hidden');
+  };
 
-    // alert('qq');
+  closeModal() {
+    this.buttonTarget.setAttribute('data-action', 'click->similar-dish#showModal');  // data-actionを元に戻す
+    this.buttonTextTarget.textContent = '似た料理を見る';
+    this.maskTarget.classList.add('hidden');
+    this.modalTarget.classList.add('hidden');
   };
 }
