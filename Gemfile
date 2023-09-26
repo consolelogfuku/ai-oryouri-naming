424105455f7e0690edea484475560f7645d2c7b8
consolelogfuku <fuku1223sugawa@gmail.com>
2023-09-26 19:44:05 +0900

    add:userのsystemSprcの実装

--- Gemfile
+++ Gemfile
@@ -104,6 +104,8 @@ group :development, :test do
   # テスト
   gem 'rspec-rails', '~> 6.0.0'
   gem 'factory_bot_rails'
+  gem 'webdrivers'
+  gem 'capybara'
 end
 
 group :development do
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,4 @@ AIOryouriNaming::Application.config.session_store :redis_store,
                                                   expire_after: 90.minutes,
                                                   key: ENV.fetch('MY_APP_SESSION', nil),
                                                   threadsafe: true,
-                                                  secure: true # 開発環境でもHTTPSを使用しているため、trueとする
-                                                  # 参考(https://github.com/redis-store/redis-rails#session-storage)
+                                                  secure: Rails.env.production? # 開発環境はHTTSだが、テスト環はHTTPSを使用しないため、無効にする
